ab4bdcdc4599bad42c6f4d427df2698ab310676f
Shriji <shrijike@gmail.com>
2020-07-11 15:48:56 +0530

    critical fix😅

--- src/App.svelte
+++ src/App.svelte
@@ -17,6 +17,7 @@
 	let username = "";
 	let articles = "";
 	let list = ""
+	let taData = "";
 	async function fetchData(x) {
 		const res = await fetch(`https://dev.to/api/articles?username=${x}&per_page=5`);
 		articles = await res.json();
@@ -28,7 +29,7 @@
 					let date = dayjs(element.published_timestamp).format("MMM DD YYYY")
 					articleList += `* ${date} [${element.title}](${element.url}) \n`
 				});
-				return articleList ? articleList : 'No articles yet!'
+				return articleList ? articleList : `Can't fetch articles`
 			};
 			return list;
 		}
@@ -44,9 +45,11 @@
 		data = await res.json();
 
 		if (res.ok) {
+			taData = `# ${data.name}\n${data.summary}\n<hr>\n### 📝 Latest articles from [dev.to](https://dev.to/${data.username})\n\n`
 			return data ? data : 'Something wrong!';
 		}
 		else {
+			taData = "bad input/username"
 			analytics.track('Logged Errors');
 			throw new Error(data);
 		}
@@ -86,25 +89,21 @@
 
 	<form>
 		<label for="username"></label>
-		<input type="text" bind:value={username} id="username" placeholder="Your dev.to username" required>
+		<input type="text" bind:value={username} id="username" placeholder="Your dev.to username" required  on:change={() => data = false}>
 
 		<button on:click={handleClick} type="submit">Fetch Data</button>
 
 	</form>
 
-
-	{#if data} 
+{#if data} 
 <p class="py-2">Copy this to your github profile's <code>README.md</code></p>
-<textarea name="md" id="mdData"># {data.name?data.name:'Have you filled the right username?'}
-{data.summary?data.summary:'Have you filled the right username?'}
-<hr>
-
-### 📝 Latest articles from [dev.to](https://dev.to/shriji) 	
+<textarea name="md" id="mdData">{taData}
 {list()}
 </textarea>
 <button on:click={copy}><svg title="Copy to clipboard" class="octicon octicon-clippy" viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true" > <path fill-rule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"></path> </svg> Copy</button>
 
 {/if}
+
 </main>
 <footer class="text-center">
 	<span>from</span> <a href="https://anoram.com">Anoram</a>
